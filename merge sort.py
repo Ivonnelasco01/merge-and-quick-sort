c8416f1e970eb12c9be137fa2c5e6eab9fbed70f
ivonne.lasco <ivonnelasco01@gmail.com>
2023-02-26 22:40:55 +0800

    merge sort

---
+++ merge sort.py
@@ -0,0 +1,53 @@
+def merge_sort(arr):
+    if len(arr) <= 1:
+        return
+
+    mid = len(arr)//2
+
+    left = arr[:mid]
+    right = arr[mid:]
+
+    merge_sort(left)
+    merge_sort(right)
+
+    merge_two_sorted_lists(left, right, arr)
+
+def merge_two_sorted_lists(a,b,arr):
+    len_a = len(a)
+    len_b = len(b)
+
+    k = 0
+    j = 0
+    i = 0
+
+    while i < len_a and j < len_b:
+        if a[i] <= b[j]:
+            arr[k] = a[i]
+            i+=1
+        else:
+            arr[k] = b[j]
+            j+=1
+        k+=1
+
+    while i < len_a:
+        arr[k] = a[i]
+        i+=1
+        k+=1
+
+    while j < len_b:
+        arr[k] = b[j]
+        j+=1
+        k+=1
+
+if __name__ == '__main__':
+    test_cases = [
+        [17, 51],
+        [],
+        [52, 31, 28],
+        [47, 50, 61, 43],
+        [65]
+    ]
+
+    for arr in test_cases:
+        merge_sort(arr)
+        print(arr)
